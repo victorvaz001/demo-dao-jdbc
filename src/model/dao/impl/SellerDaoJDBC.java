a49ef13f3f133f7577827f807f7a18c7cdf4ce9e
Victor Gonçalves Vaz <victorvaz001@gmail.com>
2020-02-05 09:10:03 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -49,16 +49,8 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {//testa se veio algum resultado
-				Department dep = new Department();//instanciando o departamento
-				dep.setId(rs.getInt("DepartmentId")); //colocar como esta no banco
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep); //objeto completo que está instanciado
+				Department dep = instanciateDepartment(rs);
+				Seller obj =  instanciateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -72,6 +64,25 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 		
 		
+	}
+
+	private Seller instanciateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep); //objeto completo que está instanciado
+		return obj;
+	}
+
+	private Department instanciateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		new Department();//instanciando o departamento
+		dep.setId(rs.getInt("DepartmentId")); //colocar como esta no banco
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
